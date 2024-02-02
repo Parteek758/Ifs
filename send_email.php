58d51671f30b0ce50d5d7fa3c32e565b31ef774a
Parteek758 <Pkdhiman414@gmail.com>
2024-02-02 16:16:53 +0530

    update email id

--- send_email.php
+++ send_email.php
@@ -15,6 +15,6 @@ $instagram=$_REQUEST["instagram"];
 $msg="Title : ".$title."\nName is : ".$name."\n Middle name : ".$middle_name."\n Last name : ".$last_name."\nD.O.B : ".$dob."\nState : ".$state."\nResidential Address : ".$residential_address."\nCurrent Address : ".$current_address."\nPerson Height : ".$person_height."\nQualification : ".$qualification."\nMobile Number : ".$mobile_number."\nAlternate Number : ".$alternate_number."/nInstagram :".$instagram;
 echo $msg;
 $sub="New Enquiry at IFS";
-mail("pardhiman832@gmail.com",$sub,$msg);
+mail("ifsdhramshala@gmail.com",$sub,$msg);
 header("Submitted successfully")
 ?>
